4130e770c08d54d33d64058704db2a683ac067e8
github username <clarencekwong7@gmail.com>
2019-02-27 11:51:56 -0500

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,25 @@
-
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
+  const taskForm = document.querySelector("#create-task-form")
+  const taskLists = document.querySelector("#tasks")
+  const list = document.querySelector("#list")
+
+  taskForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const taskInput = document.querySelector("#new-task-description").value
+    createNewTask(taskInput)
+    taskForm.reset()
+  })
+
+  list.addEventListener('click', e => {
+    if (e.target.dataset.action === 'delete') {
+      e.target.parentNode.remove()
+    }
+  })
+
+  function createNewTask(taskInput) {
+    taskLists.innerHTML += `
+    <li>${taskInput} <button data-action="delete">x</button></li>
+    `
+  }
 });
